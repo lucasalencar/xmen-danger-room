25384120f34b6f179cfb93b3889c05df48839f2d
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2017-07-18 18:34:23 -0300

    Remove default failing spec

--- spec/xmen_danger_room_spec.rb
+++ spec/xmen_danger_room_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe XmenDangerRoom do
   it "has a version number" do
     expect(XmenDangerRoom::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
